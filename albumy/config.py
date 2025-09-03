56a42a9c4af2ddc1bce92ac9c620e6336c64cb34
CheungJan <cadence_zhang@hotmail.com>
2025-09-03 10:51:48 +0800

    使用greyli仓库配置新增分支到cj远程仓库

---
+++ albumy/config.py
@@ -0,0 +1,6 @@
+MAIL_SERVER = 'smtp.example.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'your-email@example.com'
+MAIL_PASSWORD = 'your-password'
+ALBUMY_MAIL_SUBJECT_PREFIX = '[Albumy]'
